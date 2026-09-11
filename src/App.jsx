c2e636bd8c2bfa67ff0280271321817e60c3ea00
trsvar4i <tarasovarseniy0303@gmail.com>
2026-09-11 18:17:32 +0500

    Add App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Ticker from "./components/Ticker";
+import Categories from "./components/Categories";
+import Process from "./components/Process";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main id="top">
+        <Hero />
+        <Ticker />
+        <Categories />
+        <Process />
+        <About />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
