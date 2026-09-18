8821f5a18886e3c3ae5081a42d8045ba9b48584e
trsvar4i <tarasovarseniy0303@gmail.com>
2026-09-18 21:51:36 +0500

    Improve src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Hero from "./components/Hero";
 import Ticker from "./components/Ticker";
 import FreshFinds from "./components/FreshFinds";
 import Categories from "./components/Categories";
+import Trust from "./components/Trust";
 import Process from "./components/Process";
 import About from "./components/About";
+import OrderRequest from "./components/OrderRequest";
+import Faq from "./components/Faq";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -17,8 +20,11 @@ export default function App() {
         <Ticker />
         <FreshFinds />
         <Categories />
+        <Trust />
         <Process />
         <About />
+        <OrderRequest />
+        <Faq />
         <Contact />
       </main>
       <Footer />
